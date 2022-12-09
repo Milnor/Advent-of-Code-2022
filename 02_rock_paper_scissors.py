142cfeac1c50f7c8f91d92d14cb3d8ff2c4675d7
Seaver Milnor <milnor@users.noreply.github.com>
2022-12-09 03:12:46 +0100

    day two part one

---
+++ 02_rock_paper_scissors.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+def did_i_win(round):
+    try:
+        opponent = round.split(" ")[0]
+        me = round.split(" ")[1].strip()
+        #print(f"{opponent} vs {me}")
+        # Wins
+        if opponent == "A" and me == "Y":
+            #print("win!")
+            return 6
+        elif opponent == "B" and me == "Z":
+            return 6
+        elif opponent == "C" and me == "X":
+            return 6
+        # Ties
+        elif opponent == "A" and me == "X":
+            return 3
+        elif opponent == "B" and me == "Y":
+            return 3
+        elif opponent == "C" and me == "Z":
+            return 3
+        # Losses
+        else:
+            return 0
+    except:
+        # ignore the final blank line
+        #print("skipped!")
+        return 0
+
+def main():
+    with open("./input/02-1.txt", "r") as file:
+        all_data = file.readlines()
+
+        # A or X is Rock, worth 1
+        # B or Y is Paper, worth 2
+        # C or Z is Scissors, worth 3
+        # Scoring:
+        # 0 = loss
+        # 3 = draw
+        # 6 = win
+        total_score = 0
+
+        for round in all_data:
+            if "X" in round:
+                total_score += 1
+            elif "Y" in round:
+                total_score += 2
+            elif "Z" in round:
+                total_score += 3
+            total_score += did_i_win(round)
+
+        print(f"Total score: {total_score}")
+
+if __name__ == "__main__":
+    main()
